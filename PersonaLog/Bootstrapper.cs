7e770b2c4071f18a965ed911ea007fa8b7337f7b
Gaurav Chaudhari <deigue@hotmail.com>
2019-03-04 22:19:17 -0500

    Attempting to integrate MEF...

--- PersonaLog/Bootstrapper.cs
+++ PersonaLog/Bootstrapper.cs
@@ -1,12 +1,20 @@
 ﻿using System.Windows;
 using Caliburn.Micro;
 using PersonaLog.ViewModels;
+using System.ComponentModel.Composition;
+using System.ComponentModel.Composition.Hosting;
+using System.ComponentModel.Composition.Primitives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PersonaLog
 {
-    class Bootstrapper : BootstrapperBase
+    class MEFBootstrapper : BootstrapperBase
     {
-        public Bootstrapper()
+        private CompositionContainer _container;
+
+        public MEFBootstrapper()
         {
             Initialize();
         }
@@ -15,5 +23,39 @@ namespace PersonaLog
         {
             DisplayRootViewFor<ShellViewModel>();
         }
+
+        protected override void Configure()
+        {
+            _container = new CompositionContainer(new AggregateCatalog
+                                                    (AssemblySource.Instance
+                                                        .Select(x => new AssemblyCatalog(x))
+                                                        .OfType<ComposablePartCatalog>())); 
+
+            var batch = new CompositionBatch();
+
+            batch.AddExportedValue<IWindowManager>(new WindowManager());
+            batch.AddExportedValue<IEventAggregator>(new EventAggregator());
+            batch.AddExportedValue(_container);
+
+            _container.Compose(batch);
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return base.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return base.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            base.BuildUp(instance);
+        }
+
+
+        
     }
 }
---
+++ PersonaLog/Views/ShellView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using MahApps.Metro.Controls;
+
+namespace PersonaLog.Views
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class ShellView : PropertyChangedBase
+    {
+        public ShellView()
+        {
+            InitializeComponent();
+        }
+
+        private string _watermark;
+
+        public string Watermark
+        {
+            get { return _watermark; }
+            set {
+                _watermark = value;
+                NotifyOfPropertyChange(() => Watermark);
+            }
+        }
+    }
+}
